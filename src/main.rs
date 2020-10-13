e967862468f84268d5e1bc8adc3a3b6e67637513
Matt Butcher <matt.butcher@microsoft.com>
2020-10-12 20:46:15 -0600

    add lots of new things
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,21 @@ fn main() {
     println!("### Arguments ###");
     std::env::args().for_each(|a| println!("arg: {}", a));
 
-    println!("### Env Vars ###");
+    println!("\n### Env Vars ###");
     std::env::vars().for_each(|v| {
-        println!("Env Var: {:?}", v);
+        println!("{} = {}", v.0, v.1);
     });
+
+    println!("\n### STDIN ###");
+    std::io::copy(&mut std::io::stdin(), &mut std::io::stdout()).unwrap();
+
+    println!("\n### Files ###");
+    std::fs::read_dir("/").unwrap().for_each(|d| {
+        let o = d.unwrap();
+        println!("{}", o.file_name().into_string().unwrap())
+    });
+    println!(
+        "\n### Read LICENSE.txt ###\n {}",
+        std::fs::read_to_string("/LICENSE.txt").unwrap()
+    );
 }
